d8a7b6dae3f917e97f47cb63d868da5815a3438f
Aaron Bonner <aaron@blizzard.(none)>
2011-10-31 10:34:21 +0000

    Move git author config to bash_profile

--- bash_aliases
+++ bash_aliases
@@ -1,4 +1,4 @@
 alias ack="ack-grep"
-alias o="xdg-open"
+alias open="xdg-open"
 alias gzcat="zcat"
 alias phpstorm="cd /opt/PhpStorm/bin && ./PhpStorm.sh & > /dev/null 2>&1"
---
+++ setup-homedir.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Get a fully qualified path
+ABSPATH="$(cd "${0%/*}" 2>/dev/null; echo "$PWD"/"${0##*/}")"
+DIR=$(dirname $ABSPATH)
+
+# Bash / terminal
+cp bash_profile ~/.bash_profile
+ln -sf $DIR/bashrc ~/.bashrc
+ln -sf $DIR/bash_functions ~/.bash_functions
+ln -sf $DIR/bash_aliases ~/.bash_aliases
+ln -sf $DIR/gitconfig ~/.gitconfig
+ln -sf $DIR/ctags ~/.ctags
+ln -sf $DIR/inputrc ~/.inputrc
+
+# Vim
+if [ ! -d ~/.vim ]; then
+  mkdir ~/.vim
+fi
+ln -sf $DIR/vim/vimrc ~/.vimrc
+ln -sf $DIR/vim/vim/update_bundles ~/.vim/update_bundles
+ln -sf $DIR/vim/vim/colors ~/.vim/colors
+ln -sf $DIR/vim/vim/autoload ~/.vim/autoload
+
