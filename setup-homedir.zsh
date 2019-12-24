b27c2499dbb6cbbe549efb0ed0b47e81eadbb0ec
Aaron Bonner <ajbonner@gmail.com>
2019-12-23 20:43:04 -0500

    Add zsh suggestions plugin

--- setup-homedir.zsh
+++ setup-homedir.zsh
@@ -8,5 +8,6 @@ DIR=$(dirname $ABSPATH)
 ln -sf $DIR/profile ~/.profile
 ln -sf $DIR/zsh/zshrc ~/.zshrc
 git clone https://github.com/robbyrussell/oh-my-zsh ~/.oh-my-zsh
+git clone git://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 
 /usr/bin/env DIR=$DIR bash setup-common.sh
